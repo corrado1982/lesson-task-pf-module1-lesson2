47c8e64ac128c05835425d7d77c5ee5eb264d9ef
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2022-09-27 14:46:08 +0200

    reasault

---
+++ js/script.js
@@ -0,0 +1,82 @@
+//question 1
+
+var integerNumber = 8;
+var decimalNumber = 4.6;
+
+console.log(integerNumber);
+console.log(decimalNumber);
+
+//question 2
+
+var workingDays = "monday" + "-" + "tuesday" + "-" + "wednesday" + "-" + "thursday" + "-" + "friday" ;
+
+console.log(workingDays);
+
+//question 3
+
+var addiction = 9 + 3 ;
+var subtraction = 9 - 3 ;
+var multiplication = 9 * 3 ;
+var division = 9 / 3 ;
+
+console.log(addiction, subtraction, multiplication, division )
+
+
+//question 4
+
+var falseBoolean = false ;
+
+//question 5
+
+var string = "The frogs are angry." ;
+var lenghtOfString = string.length ;
+
+console.log(lenghtOfString);
+
+
+//question 6
+
+var timeElapsed;
+var totalPrice;
+
+//question 7
+
+var number1 = "53";
+var number2 = "44.6";
+
+var convertInteger = parseInt(number1);
+var convertDecimal = parseFloat(number2);
+
+console.log (convertInteger + convertDecimal)
+
+//question 8
+
+var pet = "donkey";
+var age = 89;
+var discountApplied = true;
+var timeLeft;
+
+var typeOfPet = typeof(pet);
+var typeOfAge = typeof(age);
+var typeOfDiscountApplied = typeof(discountApplied);
+var typeOfTimeLeft = typeof(timeLeft);
+
+
+
+console.log(typeOfPet);
+console.log(typeOfAge);
+console.log(typeOfDiscountApplied);
+console.log(typeOfTimeLeft);
+
+//question 9
+
+var updatingHeading = "Updating heading";
+var title = document.querySelector("h1");
+
+title.innerHTML = updatingHeading;
+
+//question 10
+
+var colorBody = document.querySelector("body");
+colorBody.style.backgroundColor = "lightskyblue";
+
